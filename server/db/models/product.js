726a1c1c834d4154c0cb889b36a0f373d4882494
Grant Horner <h.grant.horner@gmail.com>
2019-03-16 17:03:20 -0400

    set up product model

---
+++ server/db/models/product.js
@@ -0,0 +1,48 @@
+const db = require('../db-cnxn')
+
+const availabilityEnum = {
+  instock: 0,
+  backordered: 1,
+  discontinued: 2
+}
+
+const Product = db.define(
+  'product',
+  {
+    name: {
+      type: db.Sequelize.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    price: {
+      type: db.Sequelize.FLOAT,
+      allowNull: false,
+      get() {
+        const price = this.getDataValue('price')
+        const discount = this.getDataValue('discount')
+        const discPrice = (price - price * discount, 2).toFixed(2)
+        return '$' + discPrice.toString()
+      }
+    },
+    discount: {
+      type: db.Sequelize.FLOAT,
+      set() {
+        const discount = this.getDataValue('discount')
+        return discount / 100
+      }
+    },
+    availability: {
+      type: db.Sequelize.INTEGER,
+      allowNull: false
+    }
+  },
+  {
+    hooks: {
+      beforeValidate: (product, options) => {
+        product.availability = availabilityEnum[product.availability]
+      }
+    }
+  }
+)
