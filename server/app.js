7374d784d7f022db3e61fe65ce6531d15df053a1
Grant Horner <h.grant.horner@gmail.com>
2019-03-16 17:41:58 -0400

    set up backend

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
+const routes = require('./routes')
 
-app.get('/', (req, res, next) => {
-  res.send('hello')
-})
+app.use(express.json())
+app.use('/api', routes.apiRoutes)
 
 module.exports = app
---
+++ server/routes/api.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const { Product } = require('../db').models
+
+router.get('/products', (req, res, next) => {
+  Product.findAll()
+    .then(products => res.send(products))
+    .catch(next)
+})
+
+router.post('/products', (req, res, next) => {
+  Product.create(req.body)
+    .then(product => res.send(product))
+    .catch(next)
+})
+
+router.delete('/products', (req, res, next) => {
+  Product.destroy({ where: { id: req.body.id } })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
