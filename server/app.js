c25b2c051777fc97abe5d16168e170f6570f79d7
Grant Horner <h.grant.horner@gmail.com>
2019-03-17 19:31:23 -0400

    pruned logging

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const path = require('path')
-const morgan = require('morgan')
 const app = express()
 
 const routes = require('./routes')
 
 app.use(express.json())
-app.use(morgan('dev'))
 app.use(express.static(path.join('client', 'dist')))
 app.use('/api', routes.apiRoutes)
 
